0db2ee625b52e7bf915916f2c86a0557aa7461da
Daksh <ozadaksh31@gmail.com>
2023-12-09 21:01:12 +0530

    moved part 1 files in 1 folder

--- part1/modelprediction.py
+++ part1/modelprediction.py
@@ -5,5 +5,5 @@ def makePrediction(image_dir):
     print(imgModel.predict_on_image(image_dir))
 
 # placeholder way to load image
-image_dir = "test images\healthy wheat (2).JPG"
+image_dir = "part1/test images/healthy wheat (1).JPG"
 makePrediction(image_dir)
