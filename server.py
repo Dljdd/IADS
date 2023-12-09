2400171878e32e496143a8b7455a1dd0223aff5e
Dylan Moraes <dylanmoraesdljdd@gmail.com>
2023-12-09 20:51:24 +0530

    part3-complete
    added part 3 along with api for it

---
+++ server.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+
+from part3 import predictCrop as predictCropSuitability
+
+class CropData(BaseModel):
+    n: int
+    p: int
+    k: int
+    temp: int
+    humidity: int
+    ph: float
+    rainfall: int
+
+app = FastAPI()
+
+@app.post("/predict/")
+def predict_crop(data: CropData = Body(...)):
+
+    n = data.n
+    p = data.p
+    k = data.k
+    temp = data.temp
+    humidity = data.humidity
+    ph = data.ph
+    rainfall = data.rainfall
+
+    prediction = predictCropSuitability(n, p, k, temp, humidity, ph, rainfall)
+    prediction_str = str(prediction)
+    return {"predicted_crop": prediction_str}
